85339986a1605b12caf651c8881064b2d9b30bf7
Benjamin Isasmendi <70007365+Mapacheee@users.noreply.github.com>
2025-07-22 18:48:38 -0400

    added 1.21.8 support

--- src/main/java/xuan/cat/fartherviewdistance/code/ChunkIndex.java
+++ src/main/java/xuan/cat/fartherviewdistance/code/ChunkIndex.java
@@ -41,7 +41,7 @@ public final class ChunkIndex extends JavaPlugin {
         final String bukkitVersion = Bukkit.getBukkitVersion();
         final String minecraftVersion = Bukkit.getMinecraftVersion();
 
-        if (minecraftVersion.equals("1.21.7")) {
+        if (minecraftVersion.equals("1.21.7") || minecraftVersion.equals("1.21.8")) {
             ChunkIndex.branchPacket = new PacketCode();
             ChunkIndex.branchMinecraft = new MinecraftCode();
             ChunkIndex.chunkServer = new ChunkServer(ChunkIndex.configData, this, ViewShape.ROUND,
